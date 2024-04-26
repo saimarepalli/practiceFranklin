c2ef621e56a44c1fc59420ff60bd4976a28304d4
Sai Krishna Marepalli <saikrishnam@ibm.com>
2024-04-26 14:35:41 +0530

    Minor UI changes

--- blocks/formstoken/formstoken.js
+++ blocks/formstoken/formstoken.js
@@ -1,18 +1,35 @@
 export default function decorate(block) {
     const rows = [...block.children];
+    var inputfields = [];
     [...block.children].forEach((row,r)=>{
         [...row.children].forEach((col,i)=>{
              if(i==0 && r<rows.length-1) {
-                 const input = document.createElement('INPUT');
-                 input.classList.add('input');
-                 input.setAttribute("id", "in-"+col.innerHTML.replace(/ /g, ""));
+                inputfields.push(col.innerHTML.replace(/ /g, ""));
+                const outDiv = document.createElement('div');
+                outDiv.classList.add('inputField');
+                var input = null;
+                 if(col.innerHTML === 'Comments') {
+                   input = document.createElement('textarea');
+                   input.classList.add('inputtext');
+                    input.setAttribute('rows', '5');
+                    input.setAttribute('cols', '55');
+                 } else {
+                    input = document.createElement('INPUT');
+                    input.classList.add('input');
+                    input.setAttribute('type', 'text');
+                 }
                  input.setAttribute("placeholder", col.innerHTML);
-                 col.replaceWith(input);
+                 input.setAttribute("id", "in-"+col.innerHTML.replace(/ /g, ""));
+                 const inputName = document.createElement('label');
+                 inputName.classList.add('inputFieldName');
+                 inputName.innerHTML=col.innerHTML;
+                 outDiv.appendChild(inputName);
+                 outDiv.appendChild(input)
+                 col.replaceWith(outDiv);
              } else if (i==0 && r==rows.length-1) {
                 const submitButton = document.createElement('button');
                 submitButton.classList.add('submitbtn');
                 submitButton.setAttribute("id", 'submit');
-                //nextbtn.classList.add('btn-next');
                 const node = document.createTextNode(col.innerHTML);
                 submitButton.append(node);
                 col.replaceWith(submitButton);
@@ -26,37 +43,20 @@ export default function decorate(block) {
      document.getElementsByClassName('formstoken')[0].setAttribute('align','center');
 
      document.getElementById('submit').addEventListener('click', ()=>{
-        var firstName = document.getElementById("in-FirstName").value;
-        var email= document.getElementById("in-Emailid").value;
-        getToken(email);
-        sendPostRequest(firstName, email);
+        const data = {};
+        inputfields.forEach(i=>{
+            var value = document.getElementById("in-"+i).value;
+            data[i] = value;
+        });
+        sendPostRequest(data);
      });
 
-     function getToken(email) {
-        const fetchCall = fetch('https://franklin.free.beeceptor.com/getToken?id='+email);
-
-            fetchCall
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log(data.token);
-                    sessionStorage.setItem('uniqueToken', JSON.stringify(data.token));
-                });
-     }
-
-     function sendPostRequest(firstName, email) {
-        const apiUrl = 'https://franklin.free.beeceptor.com/userData';
-        let token = JSON.parse(sessionStorage.getItem('uniqueToken'));
+     function sendPostRequest(data) {
+        const apiUrl = 'https://franklin.free.beeceptor.com/contactus';
         let headers = new Headers();
-        headers.append('Authentication', `Bearer ${token}`);
         headers.append('Content-Type', 'application/json');
-        const data = {
-        firstName: firstName,
-        email: email,
-        };
-
         const requestOptions = {
         method: 'POST',
-        mode: 'cors',
         headers: headers,
         body: JSON.stringify(data),
         };
